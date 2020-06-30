6d7e46a310cb06d6c24b7ac2f59b4807f2949588
Juan Rodriguez <jrodri89@uwo.ca>
2020-06-29 20:46:03 -0400

    Made sliders display values to correct decimal places.

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderChange(_ sender: UISlider) {
+        let formatHeight = String(format: "%.2f", heightSlider.value)
+        heightLabel.text = formatHeight + "m"
+    }
+    
+    @IBAction func weightSliderChange(_ sender: UISlider) {
+        let unformatWeight = String(format: "%.0f", weightSlider.value)
+        weightLabel.text = unformatWeight + "Kg"
+    }
 }
 
